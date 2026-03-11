7512e93feabf97598ba1530d97a08d11bbc59cc6
loje0003 <loje0003@stud.kea.dk>
2026-03-11 14:43:23 +0100

    rettelser

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // harmonika
-const titles = document.querySelectorAll(".list_title");
+// const titles = document.querySelectorAll(".list_title");
 
-titles.forEach((title) => {
-  title.addEventListener("click", function () {
-    const parent = this.parentElement;
+// titles.forEach((title) => {
+//   title.addEventListener("click", function () {
+//     const parent = this.parentElement;
 
-    document.querySelectorAll(".list").forEach((item) => {
-      if (item !== parent) {
-        item.classList.remove("active");
-      }
-    });
+//     document.querySelectorAll(".list").forEach((item) => {
+//       if (item !== parent) {
+//         item.classList.remove("active");
+//       }
+//     });
 
-    parent.classList.toggle("active");
-  });
-});
+//     parent.classList.toggle("active");
+//   });
+// });
 
 // baggrund
 const root = document.documentElement;
